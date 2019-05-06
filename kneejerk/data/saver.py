982513ddfb2cfad99bda75026366726b0c77047a
NapsterInBlue <NapsterInBlue@gmail.com>
2019-05-06 12:00:22 -0400

    Add consistent output.csv ordering

--- kneejerk/data/saver.py
+++ kneejerk/data/saver.py
@@ -2,7 +2,7 @@ import csv
 
 
 def persist_data(fpaths, scores, output_path):
-    '''
+    """
     Given a list of fpaths to images, scores,
     and a place to save, generates a ``.csv``
     that will be used for later data loading
@@ -13,9 +13,10 @@ def persist_data(fpaths, scores, output_path):
         Absolute locations of images
     scores: list, numeric
     output_path: fpath-like
-    '''
+    """
 
     with open(output_path, 'w', newline='') as f:
         csvout = csv.writer(f)
-        for pair in zip(fpaths, scores):
+        for pair in sorted(zip(fpaths, scores),
+                           key=lambda x: x[0]):
             csvout.writerow(pair)
