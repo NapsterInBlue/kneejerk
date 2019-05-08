9f2f6d445dcb2a642da98a1cd0f8cd5c9583e94f
NapsterInBlue <NapsterInBlue@gmail.com>
2019-05-08 15:03:20 -0400

    BUGFIX: test/train ordering was off

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='kneejerk',
-   version='1.0.0',
+   version='1.0.1',
    description='A package for labelling image data with your quick, kneejerk reactions.',
    author='NapsterInBlue',
    author_email='NapsterInBlue@google.com',
